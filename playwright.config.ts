cf9690aab2a1cb905a01427bd664b879233757b1
vitoriarx <vitoriasantos78690@gmail.com>
2025-08-31 19:08:18 -0300

    rodando com o reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,38 +1,25 @@
 import { defineConfig, devices } from '@playwright/test';
 import * as dotenv from 'dotenv';
-
 dotenv.config();
+
+const isCI = !!process.env.CI;
 
 export default defineConfig({
   testDir: './tests',
   timeout: 30_000,
-  retries: 1,
+  retries: isCI ? 2 : 0,
   expect: { timeout: 10_000 },
-
- // playwright.config.ts
-use: {
-  headless: false,
-  trace: 'on',
-  screenshot: 'on',
-},
-
-
+  use: {
+    // baseURL: process.env.SR_BASE_URL || 'https://front.serverest.dev',
+    headless: isCI,
+    trace: isCI ? 'retain-on-failure' : 'on',
+    screenshot: isCI ? 'only-on-failure' : 'on',
+    video: isCI ? 'retain-on-failure' : 'off',
+  },
   projects: [
-    {
-      name: 'Chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-    {
-      name: 'Firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'WebKit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    { name: 'Chromium', use: { ...devices['Desktop Chrome'] } },
+    { name: 'Firefox',  use: { ...devices['Desktop Firefox'] } },
+    { name: 'WebKit',   use: { ...devices['Desktop Safari'] } },
   ],
-
-  reporter: [
-    ['html', { outputFolder: 'playwright-report', open: 'never' }],
-  ],
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
 });
